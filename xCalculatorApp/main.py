afbe0e5683504b3ec0119db09a481281b06f486d
JoeTheorium <fernandoandresraggio@gmail.com>
2023-09-20 00:22:50 -0300

    All functional

--- xCalculatorApp/main.py
+++ xCalculatorApp/main.py
@@ -1,4 +1,13 @@
 from gui import xCalculator
+import tkinter as tk  # Importo la biblioteca "tkinter" para crear la ventana principal
+
+
+def main():
+    root = tk.Tk()  # Creo la ventana principal
+    # Creo una instancia de "xCalculator" y paso 'root' como argumento
+    app = xCalculator(root)
+    root.mainloop()  # Inicio el bucle principal de la interfaz gráfica
+
 
 if __name__ == "__main__":
     main()
